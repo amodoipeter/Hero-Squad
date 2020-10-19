09054c8b30b976a491775028a748cf8c6b2cf9be
amodoipeter <amodoipeter@gmail.com>
2020-10-19 17:18:15 +0300

    Added getters and setters

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -12,12 +12,51 @@ public class Squad {
     private static ArrayList<Squad> instances = new ArrayList<>();
 
 
-    public Squad(String name, int size, String cause ){
+    public Squad(String name, int size, String cause) {
         squadName = name;
         squadSize = size;
         this.cause = cause;
         this.squadMembers = new ArrayList<>();
         instances.add(this);
         this.squadId = instances.size();
+
+    }
+        public int getSquadId () {
+            return squadId;
+        }
+        public static Squad findBySquadId ( int id){
+            return instances.get(id - 1);
+        }
+        public String getSquadName () {
+            return squadName;
+        }
+        public int getSize () {
+            return squadSize;
+        }
+        public String getCause () {
+            return this.cause;
+        }
+        public static ArrayList<Squad> getInstances () {
+            return instances;
+        }
+        public ArrayList<Hero> getSquadMembers () {
+            return squadMembers;
+        }
+        public void setSquadMembers (Hero newMember){
+            squadMembers.add(newMember);
+        }
+        public static void clearAllSquads() {
+            instances.clear();
+        }
+        public void clearAllSquadMembers () {
+            getSquadMembers().clear();
+        }
+
+        public static Squad setUpNewSquad () {
+            return new Squad("Rangers", 5, "Sexism");
+        }
+        public static Squad setUpNewSquad1 () {
+            return new Squad("Rock", 5, "Sneezing");
+        }
 
     }
