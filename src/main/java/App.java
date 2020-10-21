caaef2f11590f53a38cc3d6f26f9f470a023e0d1
amodoipeter <amodoipeter@gmail.com>
2020-10-21 17:59:11 +0300

    Added code to create a squad

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,44 +1,26 @@
-import models.Hero;
-import models.Squad;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 public class App {
 
-    public static void main(String[] args){
-
-        ProcessBuilder process = new ProcessBuilder();
-        Integer port;
-
-        if (process.environment().get("PORT") != null) {
-            port = Integer.parseInt(process.environment().get("PORT"));
-        }else {
-            port = 4567;
-        }
-        port(port);
-
-        staticFileLocation("/public");
-        Hero.setUpNewHero();
-        Hero.setUpNewHero1();
-        Squad.setUpNewSquad();
-
-        get("/", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model,"index.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/hero-form",(req, res) ->{
-            Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model, "hero-form.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/hero",(req, res) ->{
-            Map<String, Object> model = new HashMap<>();
-            ArrayList<Hero> hero = Hero.getAllInstances();
-            model.put("hero",hero);
-            return new ModelAndView(model, "hero.hbs");
-        }, new HandlebarsTemplateEngine());
+    public static void main(String[] args) {
+//        staticFileLocation("/public");
+//        Hero.setUpNewHero();
+//        Hero.setUpNewHero1();
+//        Squad.setUpNewSquad();
+//
+//        get("/", (request, response) -> {
+//            Map<String, Object> model = new HashMap<>();
+//            return new ModelAndView(model, "index.hbs");
+//        }, new HandlebarsTemplateEngine());
+//
+//        get("/hero-form", (req, res) -> {
+//            Map<String, Object> model = new HashMap<>();
+//            return new ModelAndView(model, "hero-form.hbs");
+//        }, new HandlebarsTemplateEngine());
+//
+//        get("/hero", (req, res) -> {
+//            Map<String, Object> model = new HashMap<>();
+//            ArrayList<Hero> hero = Hero.getAllInstances();
+//            model.put("hero", hero);
+//            return new ModelAndView(model, "hero.hbs");
+//        }, new HandlebarsTemplateEngine());
     }
 }
