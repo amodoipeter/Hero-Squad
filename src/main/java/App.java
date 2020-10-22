fee15f8255edc74621d3c2fb97e35dc85449e2cf
amodoipeter <amodoipeter@gmail.com>
2020-10-22 07:25:30 +0300

    Added form for squad details

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,26 +1,38 @@
+import models.Hero;
+import models.Squad;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
 
     public static void main(String[] args) {
-//        staticFileLocation("/public");
-//        Hero.setUpNewHero();
-//        Hero.setUpNewHero1();
-//        Squad.setUpNewSquad();
-//
-//        get("/", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            return new ModelAndView(model, "index.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//        get("/hero-form", (req, res) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            return new ModelAndView(model, "hero-form.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//        get("/hero", (req, res) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            ArrayList<Hero> hero = Hero.getAllInstances();
-//            model.put("hero", hero);
-//            return new ModelAndView(model, "hero.hbs");
-//        }, new HandlebarsTemplateEngine());
+        staticFileLocation("/public");
+        Hero.setUpNewHero();
+        Hero.setUpNewHero1();
+        Squad.setUpNewSquad();
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/hero-form", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/hero", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> hero = Hero.getAllInstances();
+            model.put("hero", hero);
+            return new ModelAndView(model, "hero.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
